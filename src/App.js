ca50d0274a74a19895bda274776ed812338a15d4
Duncan Bain <duncan@bain.io>
2020-01-18 18:08:14 +0000

    Added decimcal and display handling functions

--- src/App.js
+++ src/App.js
@@ -8,13 +8,15 @@ class App extends Component {
 
     this.state = {
       currentValue: '0',
-      currentTotal: '',
+      hasDecimal: false
     }
 
-    this.handleCalculatorCLear = this.handleCalculatorCLear.bind(this);
+    this.handleCalculatorCLear = this.handleCalculatorClear.bind(this);
+    this.handleDisplayedValue = this.handleDisplayedValue.bind(this);
+    this.handleDisplayDecimal = this.handleDisplayDecimal.bind(this);
   }
 
-  handleCalculatorCLear() {
+  handleCalculatorClear() {
     this.setState({
       currentValue: '0',
       currentTotal: ''
@@ -22,8 +24,21 @@ class App extends Component {
   }
 
   handleDisplayedValue(value) {
-    
+    if(value === '0'){
+      this.setState({currentValue: this.state.currentValue === '0' ? '0' : this.state.currentValue + value});
+    } else {
+      this.setState({currentValue: this.state.currentValue === '0' ? value : this.state.currentValue + value});
+    }  
+  } 
+
+  handleDisplayDecimal() {
+    if(this.state.hasDecimal === false){
+      this.setState({currentValue: this.state.currentValue + '.',
+                     hasDecimal: true
+                    });
+    }
   }
+
   
   
   render() {
@@ -33,38 +48,38 @@ class App extends Component {
       <div className="container">
       <div className="level">
           <div className="level-item">
-            <h2 className="has-background-black has-text-danger" id="display">{this.state.displayedValue}</h2>
+            <h2 className="has-background-black has-text-danger" id="display">{this.state.currentValue}</h2>
           </div>
         </div>
         <div className="level is-marginless is-paddingless">
           <div className="level-item">
-            <button className="button" id="seven" onClick={() => this.handleDisplayedValue(7)}>7</button>
-            <button className="button" id="eight"onClick={() => this.handleDisplayedValue(8)}>8</button>
-            <button className="button" id="nine" onClick={() => this.handleDisplayedValue(9)}>9</button>
+            <button className="button" id="seven" onClick={() => this.handleDisplayedValue('7')}>7</button>
+            <button className="button" id="eight"onClick={() => this.handleDisplayedValue('8')}>8</button>
+            <button className="button" id="nine" onClick={() => this.handleDisplayedValue('9')}>9</button>
             <button className="button" id="multiply">X</button>
           </div>
         </div>
         <div className="level is-marginless is-paddingless">
           <div className="level-item">
-            <button className="button" id="four" onClick={() => this.handleDisplayedValue(4)}>4</button>
-            <button className="button" id="five"onClick={() => this.handleDisplayedValue(5)}>5</button>
-            <button className="button" id="six"onClick={() => this.handleDisplayedValue(6)}>6</button>
+            <button className="button" id="four" onClick={() => this.handleDisplayedValue('4')}>4</button>
+            <button className="button" id="five"onClick={() => this.handleDisplayedValue('5')}>5</button>
+            <button className="button" id="six"onClick={() => this.handleDisplayedValue('6')}>6</button>
             <button className="button" id="subtract">-</button>
           </div>
         </div>
         <div className="level is-marginless is-paddingless">
           <div className="level-item">
-            <button className="button" id="one"onClick={() => this.handleDisplayedValue(1)}>1</button>
-            <button className="button" id="two"onClick={() => this.handleDisplayedValue(2)}>2</button>
-            <button className="button" id="three"onClick={() => this.handleDisplayedValue(3)}>3</button>
+            <button className="button" id="one"onClick={() => this.handleDisplayedValue('1')}>1</button>
+            <button className="button" id="two"onClick={() => this.handleDisplayedValue('2')}>2</button>
+            <button className="button" id="three"onClick={() => this.handleDisplayedValue('3')}>3</button>
             <button className="button" id="add">+</button>
           </div>
         </div>
         <div className="level is-marginless is-paddingless">
           <div className="level-item">
-            <button className="button" id="zero">0</button>
-            <button className="button" id="decimal">.</button>
-            <button className="button" id="clear" onClick={this.clearCalculator}>AC</button>
+            <button className="button" id="zero" onClick={() => this.handleDisplayedValue('0')}>0</button>
+            <button className="button" id="decimal" onClick={() => this.handleDisplayDecimal()}>.</button>
+            <button className="button" id="clear" onClick={this.handleCalculatorCLear}>AC</button>
             <button className="button" id="divide">/</button>
             <button className="button" id="equals">=</button>
           </div>
