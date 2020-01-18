d0eea43c37280dd2aa864515bff15ff991e17e03
Duncan Bain <duncan@bain.io>
2020-01-18 15:48:10 +0000

    Static layout complete

--- src/App.js
+++ src/App.js
@@ -1,38 +1,51 @@
 import React from 'react';
 import 'bulma/css/bulma.css'
+import ReactFCCtest from 'react-fcctest';
 
 function App() {
   return (
     <div className="App">
-
-<h1 className="title">Bulma</h1>
-<p className="subtitle">
-  Modern CSS framework based on{' '}
-  <a href="https://developer.mozilla.org/en-US/docs/Web/CSS/CSS_Flexible_Box_Layout/Basic_Concepts_of_Flexbox">
-    Flexbox
-  </a>
-</p>
-
-<div className="field">
-  <div className="control">
-    <input className="input" type="text" placeholder="Input" />
-  </div>
-</div>
-
-<div className="field">
-  <p className="control">
-    <span className="select">
-      <select>
-        <option>Select dropdown</option>
-      </select>
-    </span>
-  </p>
-</div>
-
-<div className="buttons">
-  <a className="button is-primary" href="blah.com">Primary</a>
-  <a className="button is-link" href="blah.com">Link</a>
-</div>
+       <ReactFCCtest />
+      <div className="container">
+      <div className="level">
+          <div className="level-item">
+            <h2 className="has-background-black has-text-danger" id="display">Calculator Display</h2>
+          </div>
+        </div>
+        <div className="level is-marginless is-paddingless">
+          <div className="level-item">
+            <button className="button" id="seven">7</button>
+            <button className="button" id="eight">8</button>
+            <button className="button" id="nine">9</button>
+            <button className="button" id="multiply">X</button>
+          </div>
+        </div>
+        <div className="level is-marginless is-paddingless">
+          <div className="level-item">
+            <button className="button" id="four">4</button>
+            <button className="button" id="five">5</button>
+            <button className="button" id="six">6</button>
+            <button className="button" id="subtract">-</button>
+          </div>
+        </div>
+        <div className="level is-marginless is-paddingless">
+          <div className="level-item">
+            <button className="button" id="one">1</button>
+            <button className="button" id="two">2</button>
+            <button className="button" id="three">3</button>
+            <button className="button" id="add">+</button>
+          </div>
+        </div>
+        <div className="level is-marginless is-paddingless">
+          <div className="level-item">
+            <button className="button" id="zero">0</button>
+            <button className="button" id="decimal">.</button>
+            <button className="button" id="clear">AC</button>
+            <button className="button" id="divide">/</button>
+            <button className="button" id="equals">=</button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
