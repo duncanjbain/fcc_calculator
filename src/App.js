f1bf935a31f9b73d8660bc6e0b8c04567b3152c8
Duncan Bain <duncan@bain.io>
2020-01-19 23:26:52 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -17,12 +17,99 @@ class App extends Component {
       previousValue: '0',
       formula: '',
       currentSign: 'positive',
-      lastValue: ''
+      lastValue: '',
+      evaluated: false,
     }
 
   }
 
+  handleCalculatorClear() {
+    this.setState({
+      currentValue: '0',
+      previousValue: '0',
+      formula: '',
+      currentSign: 'positive',
+      lastValue: '',
+      evaluated: false,
+    })
+  }
+
+  handleEvaluation() {
+    let expressionToEvalate = this.state.formula;
+    while(endsWithOperand.test(expressionToEvalate)) {
+      expressionToEvalate = expressionToEvalate.slice(0,-1);
+    }
+    let answer = Math.round(1000000000000 * evaluate(expressionToEvalate)) / 1000000000000;
+    this.setState({
+      currentValue: answer.toString(),
+      formula: expressionToEvalate.replace(/\*/g, "⋅").replace(/-/g, "‑") + "=" + answer,
+      previousValue: answer,
+      evaluated: true,
+    })
+  }
+
+  handleNumber(value) {
+    if(this.state.evaluated) {
+      this.setState({
+        currentValue: value,
+        formula: value !== '0' ? value : ''
+      })
+    } else {
+      this.setState({
+        currentValue: this.state.currentValue === '0' || isOperand.test(this.state.currentValue) ?
+        value : this.state.currentValue + value,
+        formula: this.state.currentValue === '0' && value === '0' ?
+        this.state.formula === '' ? value : this.state.formula : /([^.0-9]0|^0)$/.test(this.state.formula) ? this.state.formula.slice(0,-1) + value
+        : this.state.formula + value
+      })
+    }
+  }
   
+  handleOperands(operand) {
+    if(this.state.evaluated) {
+      this.setState({
+        formula: this.state.previousValue + operand
+      });
+    } else if(!endsWithOperand.test(this.state.formula)) {
+      this.setState({
+        previousValue: this.state.formula,
+        formula: this.state.formula + operand
+      });
+    } else if(!endsWithNegativeOperand.test(this.state.formula)) {
+      this.setState({
+        formula: (endsWithNegativeOperand.test(this.state.formula + operand) ? 
+        this.state.formula : this.state.previousValue) + operand
+      });
+    } else if(operand !== '-') {
+      this.setState({
+        formula: this.state.previousValue + operand
+      })
+    }
+  }
+
+  handleDecimal() {
+    if(this.state.evaluated===true) {
+      this.setState({
+        currentValue: '0.',
+        formula: '0.',
+        evaluated: false
+      });
+    } else if(endsWithOperand.test(this.state.formula) || (this.state.currentValue === '0' && this.state.formula === '')) {
+      console.log(endsWithOperand.test(this.state.formula))
+      this.setState({
+        currentValue: '0.',
+        formula: this.state.formula + '0.'
+      });
+    } else {
+      console.log('do else on decimal');
+      this.setState({
+        
+        currentValue: this.state.formula.match(/(-?\d+\.?\d*)$/)[0] + '.',
+        formula: this.state.formula + '.'
+      })
+    }
+  }
+
 
   render() {
   return (
@@ -31,9 +118,11 @@ class App extends Component {
       <div className="container">
       <div className="level">
           <div className="level-item">
-            <h2 className="has-background-black has-text-danger" id="display">{this.state.currentValue}</h2>
-            <h2 className="has-background-black has-text-danger" id="formula">{this.state.formula}</h2>
+            <h1 className="has-background-black has-text-danger" id="display">{this.state.currentValue}</h1>
           </div>
+        </div>
+        <div className="level-item">
+        <h2 className="has-background-black has-text-danger" id="formula">{this.state.formula}</h2>
         </div>
         <div className="level is-marginless is-paddingless">
           <div className="level-item">
@@ -65,7 +154,7 @@ class App extends Component {
             <button className="button" id="decimal" onClick={() => this.handleDecimal()}>.</button>
             <button className="button" id="clear" onClick={() => this.handleCalculatorClear()}>AC</button>
             <button className="button" id="divide" onClick={() => this.handleOperands('/')}>/</button>
-            <button className="button" id="equals" onClick={this.handleCalculation}>=</button>
+            <button className="button" id="equals" onClick={() => this.handleEvaluation()}>=</button>
           </div>
         </div>
       </div>
